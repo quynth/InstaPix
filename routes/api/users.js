19215e588e750dfb79f9c116fce00805d979bf37
Quynh <71189807+quynth@users.noreply.github.com>
2021-07-21 13:31:32 -0700

    generated token

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,46 +1,83 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const User = require('../../models/user');
+const keys = require('../../config/keys');
 
 //@route POST api/users/register
 //@desc Register a users
 //@access public
 
 router.post('/register', (req, res) => {
-  User.findOne({email: req.body.email})
-    .then(user => {
-      if(user) {
-        return res.status(400).json({email:'Email already exists'});
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (user) {
+        return res.status(400).json({ email: 'Email already exists' });
       } else {
-        
-        const avatar = gravatar.url(req.body.email,{
+        const avatar = gravatar.url(req.body.email, {
           s: '200',
           r: 'pg',
-          d: 'mm'
+          d: 'mm',
         });
 
         const newUser = new User({
           name: req.body.name,
           email: req.body.email,
           password: req.body.password,
-          avatar
+          avatar,
         });
 
-        bcrypt.genSalt(10,(err,salt) => {
-          if(err) throw err;
-          bcrypt.hash(req.body.password,salt,(err,hash) => {
-            if(err) throw err;
+        bcrypt.genSalt(10, (err, salt) => {
+          if (err) throw err;
+          bcrypt.hash(req.body.password, salt, (err, hash) => {
+            if (err) throw err;
             newUser.password = hash;
-            newUser.save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err))
+            newUser
+              .save()
+              .then((user) => res.json(user))
+              .catch((err) => console.log(err));
           });
         });
       }
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
+//@route POST api/users/login
+//@desc Login a user and generate a token
+//@access public
+
+router.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ email: 'User not found' });
+      }
+      bcrypt.compare(req.body.password, user.password).then((isMatch) => {
+        if (isMatch) {
+          //Payload
+          const payload = {
+            id: user.id,
+            name: user.name,
+            avatar: user.avatar,
+          };
+
+          //Sign token
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 3600 },
+            (err, token) => {
+              return res.json({ token: 'Bearer ' + token });
+            }
+          );
+        } else {
+          return res.status(400).json({ password: 'Incorrect password' });
+        }
+      });
+    })
+    .catch((err) => console.log(err));
+});
 module.exports = router;
