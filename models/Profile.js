9955288f1fa23d3ca9c487b8693c5d2b976e748d
i67ahmed <i67ahmed@gmail.com>
2021-07-27 12:57:06 -0400

    profile model and routes

---
+++ models/Profile.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  bio: {
+    type: String
+  },
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }   
+  }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
