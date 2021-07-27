84f8395f91a9b062d225b0070831dfa377ec206b
Quynh <71189807+quynth@users.noreply.github.com>
2021-07-26 19:21:41 -0700

    completed login validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,8 +6,8 @@ const passport = require('passport');
 const router = express.Router();
 const User = require('../../models/User');
 const keys = require('../../config/keys');
-const validateRegisterInput = require('../../validation/register')
-
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 //@route POST api/users/register
 //@desc Register a users
@@ -15,14 +15,14 @@ const validateRegisterInput = require('../../validation/register')
 
 router.post('/register', (req, res) => {
   //validation
-  const {errors, isValid} = validateRegisterInput(req.body);
-  
-  if(!isValid){
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
     return res.status(400).json(errors);
   }
 
   User.findOne({ email: req.body.email })
-    .then(user => {
+    .then((user) => {
       if (user) {
         return res.status(400).json({ email: 'Email already exists' });
       } else {
@@ -36,7 +36,7 @@ router.post('/register', (req, res) => {
           name: req.body.name,
           email: req.body.email,
           password: req.body.password,
-          avatar
+          avatar,
         });
 
         bcrypt.genSalt(10, (err, salt) => {
@@ -46,8 +46,8 @@ router.post('/register', (req, res) => {
             newUser.password = hash;
             newUser
               .save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err))
+              .then((user) => res.json(user))
+              .catch((err) => console.log(err));
           });
         });
       }
@@ -60,13 +60,19 @@ router.post('/register', (req, res) => {
 //@access public
 
 router.post('/login', (req, res) => {
+  //validation
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email })
-    .then(user => {
+    .then((user) => {
       if (!user) {
         return res.status(404).json({ email: 'User not found' });
       }
-      bcrypt.compare(req.body.password, user.password)
-      .then(isMatch => {
+      bcrypt.compare(req.body.password, user.password).then((isMatch) => {
         if (isMatch) {
           //Payload
           const payload = {
@@ -82,13 +88,14 @@ router.post('/login', (req, res) => {
             { expiresIn: 3600 },
             (err, token) => {
               return res.json({ token: 'Bearer ' + token });
-            })
+            }
+          );
         } else {
           return res.status(400).json({ password: 'Incorrect password' });
         }
-      })
+      });
     })
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err));
 });
 
 //@route POST api/users/current
@@ -96,10 +103,10 @@ router.post('/login', (req, res) => {
 //@access Private
 router.get(
   '/current',
-  passport.authenticate('jwt', {session:false}),
+  passport.authenticate('jwt', { session: false }),
   (req, res) => {
-   res.json(req.user);
-  });
-
+    res.json(req.user);
+  }
+);
 
 module.exports = router;
