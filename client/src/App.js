d6005c8cdd7f2adadc36b5543c53f244035f1984
prachi84 <prachi8384@gmail.com>
2021-08-02 13:59:15 -0700

    Added Navbar.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import './App.css';
 import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
 
 
 class App extends Component {
@@ -8,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Navbar />
         <Footer />
       </div>
     );
