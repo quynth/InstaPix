6898886b3d07767280673a2e1c130cff0f461112
prachi84 <prachi8384@gmail.com>
2021-08-02 14:20:34 -0700

    change the title to InstaPix

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,7 +5,7 @@ class Navbar extends Component {
     return (
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
-          <a className="navbar-brand" href="landing.html">DevConnector</a>
+          <a className="navbar-brand" href="landing.html">Insta Pix</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -13,7 +13,7 @@ class Navbar extends Component {
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <a className="nav-link" href="profiles.html"> Developers
+                <a className="nav-link" href="profiles.html"> 
                 </a>
               </li>
             </ul>
