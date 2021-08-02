9c509e19781152085e612141d97c0e5980a4379e
i67ahmed <i67ahmed@gmail.com>
2021-08-02 14:45:05 -0400

    Landing.js set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import './App.css';
 import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 
 
 class App extends Component {
@@ -8,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Landing />
         <Footer />
       </div>
     );
