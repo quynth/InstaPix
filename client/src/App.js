d416da1850b2f2a594186d3a74815fc3a94635d9
fangye66 <fangye.yf@gmail.com>
2021-08-02 01:19:42 -0400

    add footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
+import React, {Component} from 'react'
 import './App.css';
+import Footer from './components/layout/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>InstaPix App</h1>
-    </div>
-  );
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+
+        <Footer />
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
