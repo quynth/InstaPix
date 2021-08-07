88bf8c8678db54dfc41f45723db3b4c4626bd1c6
prachi84 <prachi8384@gmail.com>
2021-08-07 13:33:00 -0700

    Installed Router dom library and Created Auth folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
@@ -9,11 +10,13 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Router exact path="/" component={Landing} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 
