a69b48b0b190a8d2ce2927815fa82afa5b52363c
Quynh <71189807+quynth@users.noreply.github.com>
2021-08-07 22:13:37 -0700

    Added links to nav bar and in state values to reg

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
- class Register extends Component {
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      password2: '',
+      errors: {},
+    };
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
     return (
       <div className="register">
@@ -8,28 +23,59 @@ import React, { Component } from 'react'
           <div className="row">
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Sign Up</h1>
-              <p className="lead text-center">Create your DevConnector account</p>
+              <p className="lead text-center">Create your InstaPix account</p>
               <form action="create-profile.html">
                 <div className="form-group">
-                  <input type="text" className="form-control form-control-lg" placeholder="Name" name="name" required />
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Name"
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.onChange.bind(this)}
+                  />
                 </div>
                 <div className="form-group">
-                  <input type="email" class="form-control form-control-lg" placeholder="Email Address" name="email" />
-                  <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChange.bind(this)}
+                  />
+                  <small className="form-text text-muted">
+                    This site uses Gravatar so if you want a profile image, use
+                    a Gravatar email
+                  </small>
                 </div>
-                <div class="form-group">
-                  <input type="password" class="form-control form-control-lg" placeholder="Password" name="password" />
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange.bind(this)}
+                  />
                 </div>
-                <div class="form-group">
-                  <input type="password" class="form-control form-control-lg" placeholder="Confirm Password" name="password2" />
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Confirm Password"
+                    name="password2"
+                    value={this.state.password2}
+                    onChange={this.onChange.bind(this)}
+                  />
                 </div>
-                <input type="submit" class="btn btn-info btn-block mt-4" />
+                <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react'
-import {Link, link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, link } from 'react-router-dom';
 class Landing extends Component {
   render() {
     return (
       <div className="landing">
-      <div className="dark-overlay landing-inner text-light">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12 text-center">
-              <h1 className="display-3 mb-4">InstaPix
-              </h1>
-              <p className="lead"> Share your photos with your community!</p>
-              <hr />
-              <Link to="register.html" className="btn btn-lg btn-info mr-2">Sign Up</Link>
-              <Link href="login.html" className="btn btn-lg btn-light">Login</Link>
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">InstaPix</h1>
+                <p className="lead"> Share your photos with your community!</p>
+                <hr />
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </Link>
+                <Link href="login.html" className="btn btn-lg btn-light">
+                  Login
+                </Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    )
+    );
   }
 }
 
