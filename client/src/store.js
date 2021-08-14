3b82f921c572b50d60bd7e685cf36f421824744a
prachi84 <prachi8384@gmail.com>
2021-08-14 16:53:40 -0700

    installed thunk ,added  const middleware and authreducer

--- client/src/store.js
+++ client/src/store.js
@@ -1,13 +1,15 @@
-import {createStore, applyMiddleware,compose} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const store = createStore(  
-  rootReducer,
+const middleware = [thunk];
+const store = createStore(
+  rootReducer, 
   {}, 
   compose(
-    applyMiddleware(),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
 
 export default store;
