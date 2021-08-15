f72c1ed57542ddc8e483cb90590db3e00b236413
Quynh <71189807+quynth@users.noreply.github.com>
2021-08-14 17:15:40 -0700

    created action types and authReducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,12 +1,19 @@
+import { SET_USER } from '../actions/types';
+
 const initialState = {
   isAuthenticated: false,
-  user: {}
+  user: {},
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default function(state=initialState, action){
-  switch(action.type){
-     default:
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case SET_USER:
+      return {
+        ...state,
+        user: action.payload,
+      };
+    default:
       return state;
   }
 }
