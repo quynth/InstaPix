4f1d2ff740f308e35750c9ed46cdfe5cf904513c
fangye66 <fangye.yf@gmail.com>
2021-08-15 01:58:29 -0400

    add this.props

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -30,10 +30,11 @@ class Register extends Component {
       password2: this.state.password2,
     };
 
-    axios
-      .post('/api/users/register', newUser)
-      .then((res) => console.log(res.data))
-      .catch((err) => this.setState({ errors: err.response.data }));
+  this.props.registerUser(newUser);
+   // axios
+   //  .post('/api/users/register', newUser)
+   //  .then((res) => console.log(res.data))
+  //   .catch((err) => this.setState({ errors: err.response.data }));
   }
 
   render() {
