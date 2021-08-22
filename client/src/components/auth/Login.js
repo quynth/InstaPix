1bf5742e06161a9f304e3b6b6825df0d6e68d344
Quynh <71189807+quynth@users.noreply.github.com>
2021-08-22 00:38:31 -0700

    login functionality

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import classnames from 'classnames';
-import {loginUser} from '../../actions/authActions';
+import { loginUser } from '../../actions/authActions';
 
 class Login extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       email: '',
       password: '',
-      errors: {}
-    }
+      errors: {},
+    };
   }
 
-  onChange(e){
-    this.setState({[e.target.name]: e.target.value})
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
   }
 
-  
-  onSubmit(e){
+  onSubmit(e) {
     e.preventDefault();
 
     const newUser = {
@@ -29,45 +28,58 @@ class Login extends Component {
     };
 
     this.props.loginUser(newUser);
-
   }
 
-  
-  componentWillReceiveProps(nextProps){
-    if (nextProps.errors){
-      this.setState({errors: nextProps.errors});
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
     }
   }
-  
+
   render() {
-    const {errors} = this.state;
+    const { errors } = this.state;
     return (
       <div className="login">
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Log In</h1>
-              <p className="lead text-center">Sign in to your InstaPix account</p>
+              <p className="lead text-center">
+                Sign in to your InstaPix account
+              </p>
               <form noValidate onSubmit={this.onSubmit.bind(this)}>
                 <div className="form-group">
-                  <input type="email"
-                  className={classnames('form-control form-control-lg', {'is-invalid': errors.email})} 
-                  placeholder="Email Address" name="email"
-                  value={this.state.email} 
-                  onChange={this.onChange.bind(this)}
-                   />
+                  <input
+                    type="email"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.email,
+                    })}
+                    placeholder="Email Address"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChange.bind(this)}
+                  />
                   {errors.email && (
-                  <div className="invalid-feedback">{errors.email}</div>)}
+                    <div className="invalid-feedback">{errors.email}</div>
+                  )}
                 </div>
                 <div className="form-group">
-                  <input type="password" 
-                  className={classnames('form-control form-control-lg', {'is-invalid': errors.password})} 
-                  placeholder="Password" name="password"
-                  value={this.state.password} 
-                  onChange={this.onChange.bind(this)}
-                   />
+                  <input
+                    type="password"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password,
+                    })}
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange.bind(this)}
+                  />
                   {errors.password && (
-                  <div className="invalid-feedback">{errors.password}</div>)}
+                    <div className="invalid-feedback">{errors.password}</div>
+                  )}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
@@ -75,18 +87,19 @@ class Login extends Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-
 Login.propTypes = {
   loginUser: PropTypes.func.isRequired,
-  errors: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-  errors: state.errors
-})
+  auth: state.auth,
+  errors: state.errors,
+});
 
-export default connect(mapStateToProps, {loginUser})(withRouter(Login));
+export default connect(mapStateToProps, { loginUser })(withRouter(Login));
