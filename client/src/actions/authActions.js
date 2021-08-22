e01d710789be582985acac99214549cb9f2eebc4
prachi84 <prachi8384@gmail.com>
2021-08-21 18:41:19 -0700

    installed jwt-decodeand created validation\is -empty.js

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
-import { GET_ERRORS, SET_USER } from './types'
 import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import { GET_ERRORS, SET_CURRENT_USER} from './types'
+import setAuthToken from '../utils/setAuthToken';
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
@@ -21,8 +23,18 @@ export const loginUser = userData => dispatch => {
     //save the token to the local storage
     const {token} = res.data.token;
     localStorage.setItem('jwtToken',token);
+    
     //set the token to the auth header
+    setAuthToken(token);
+
+    //Decode the token
+    const decoded = jwt_decode(token);
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: decoded
+    })
    })
+
   .catch(err => 
    dispatch({
      type:GET_ERRORS,
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,4 +1,5 @@
-import { SET_USER } from '../actions/types';
+import isEmpty from '../validation/is-empty';
+import { SET_CURRENT_USER } from '../actions/types';
 
 const initialState = {
   isAuthenticated: false,
@@ -8,9 +9,10 @@ const initialState = {
 // eslint-disable-next-line import/no-anonymous-default-export
 export default function (state = initialState, action) {
   switch (action.type) {
-    case SET_USER:
+    case SET_CURRENT_USER:
       return {
         ...state,
+        isAuthenticated: !isEmpty(action.payload),
         user: action.payload,
       };
     default:
